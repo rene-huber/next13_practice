62c1f381872365d63bae177917a34309b92f558c
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-21 21:07:50 +0100

    setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import AuthProvider from '@/providers/AuthProvider'
+import ToasterContext from '@/providers/ToasterProvider'
+import Navbar from './components/navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+<ToasterContext />
+<Navbar />
+        {children}
+        </AuthProvider>
+        </body>
     </html>
   )
 }
