d8a428db1b63ba19a8561a0af2f4cb825c550589
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-22 22:17:15 +0100

    next auth setup

---
+++ 22-12/src/utils/auth.js
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "./prismaConnect";
+import { getServerSession } from "next-auth";
+
+export const authOptions = {
+ 
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
