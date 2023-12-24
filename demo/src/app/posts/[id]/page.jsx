9d02971245f2ed1b23797e63baaa5763a0efbf94
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-24 06:44:31 +0100

    demo

---
+++ demo/src/app/posts/[id]/page.jsx
@@ -0,0 +1,20 @@
+import Params from '@/app/components/params/Params'
+import Image from 'next/image';
+import styles from './singlePage.module.css'
+import prisma from "@/utils/prismaConnect";
+
+
+
+
+const OnePost = async (req , { params }) => {
+
+
+console.log(params, 'params');
+ 
+  
+  }
+
+
+
+
+export default OnePost
