6f65677da081c6bb245873b9ad3c58e32e9940bf
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-24 02:50:49 +0100

    image fail

---
+++ credential/src/components/navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+'use client'
+import React, { useState } from 'react'
+import css from "./navbar.module.css";
+import Image from "next/image";
+import noPicture from "../../../public/noImage.png";
+import dancingbaby from "../../../public/dancingbaby.gif";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import {signIn, signOut, useSession} from 'next-auth/react'
+
+const Navbar = () => {
+
+  const [showDropdown, setShowDropdown] = useState(false)
+  const {data: session} = useSession()
+
+  const handleShowDropdown = () => setShowDropdown(prev => true)
+  const handleHideDropdown = () => setShowDropdown(prev => false)
+ 
+console.log(session?.user)
+
+  return (
+    <div className={css.container}>
+      <div className={css.social}>
+{
+  session?.user ? (
+    <Image src={dancingbaby} alt="user image" width={40} height={40} className={css.avatar} />
+  ) : ( <p className={css.notSigned}>Social</p>
+  )
+}
+
+   
+      
+      </div>
+
+      <div className={css.userEmail}>
+      {
+            session?.user
+              ? ( <p>{session?.user?.email}</p>)
+              : (<p className={css.notSigned}>Not signed in</p>)
+          }
+      </div>
+      <div className={css.links}>
+        <Link href="/" className={css.link}>Home</Link>
+        <Link href="/posts" className={css.link}>Posts</Link>
+        <Link href="/dashboard" className={css.link}>dashboard</Link>
+        
+        <AuthLinks />
+        {
+            session?.user
+              ? ( <Image src={session?.user?.image} alt="user image" width={40} height={40} className={css.avatar} />)
+              : (<Image src={noPicture} alt="user image" width={40} height={40} className={css.avatar} />)
+          }
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
