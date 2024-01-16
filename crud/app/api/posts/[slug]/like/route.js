1b45fb9b749601b294a7ab7a4f25129146677042
rene huber <huberskull@gmail.com>
2024-01-16 23:01:38 +0100

    wrong push - patch

--- crud/app/api/posts/[slug]/like/route.js
+++ crud/app/api/posts/[slug]/like/route.js
@@ -1,3 +1,33 @@
 import { getAuthSession } from "@/utils/auth";
+import { getCurrentUser } from '@/utils/session';
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/prismaConnect";
 import { NextResponse } from "next/server";
+
+
+
+export const PUT = async (req,{ params}) => {
+    const  {slug} = params;
+    const session = await getCurrentUser();
+    const userEmail = session?.user?.email;
+  
+    if (!session) {
+      return new NextResponse(
+        JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+      );
+    }
+  
+    try {
+     
+        const post = await prisma.post.update({ where: { slug: req.query.slug }});
+  
+      return new NextResponse(JSON.stringify(post, { status: 200 }));
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+      );
+    }
+  };
+  
+  
