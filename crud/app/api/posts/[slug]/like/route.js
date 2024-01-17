4b5374fb82cce5767d511cced190e36622f56867
rene-huber <96597641+rene-huber@users.noreply.github.com>
2024-01-17 13:16:37 +0100

    schema like

--- crud/app/api/posts/[slug]/like/route.js
+++ crud/app/api/posts/[slug]/like/route.js
@@ -6,6 +6,51 @@ import { NextResponse } from "next/server";
 
 
 
+export const POST = async (req,{ params}) => {
+  const  {slug} = params;
+  const session = await getCurrentUser();
+  const userEmail = session?.user?.email;
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
+
+  try {
+    
+    let like = await prisma.like.findUnique({
+      where: {
+        postSlug_userEmail: {
+          postSlug: slug,
+          userEmail: userEmail,
+        },
+      },
+    });
+
+    if (like) {
+      await prisma.like.delete({ where: { id: like.id } });
+    } else {
+      like = await prisma.like.create({
+        data: { postSlug: slug, userEmail: userEmail },
+      });
+    }
+    
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
+
+
+
+
+
+
+
 export const PUT = async (req,{ params}) => {
     const  {slug} = params;
     const session = await getCurrentUser();
